c88b785373e14b2f6d839856fa20b248e6c21aa0
lui <lppbrancher@senacrs.com.br>
2024-02-23 20:58:03 -0300

    update oda prato saudavel

--- objetos/prato-saudavel-1.0v/assets/js/prato_saudavel.js
+++ objetos/prato-saudavel-1.0v/assets/js/prato_saudavel.js
@@ -10,83 +10,49 @@ $('.start_btn').click(function () {
         $('.tela-2').fadeIn()
     }, 500)
 })
-
+const audio = new Audio();
 // Lógica do Objeto:
-let selected = 0;
 let corretosTotal = 0;
+$('.option-e').click(function () {
+    audio.setAttribute('src', 'assets/audio/erro.mp3');
+    audio.load();
+    audio.play();
+    telaAtual = $(this).parents('.tela');
+    proxTela = $(this).parents('.tela').next();
+    telaAtual.fadeOut();
+    proxTela.fadeIn();
+    if (telaAtual.hasClass('tela-4')) {
+        // resultado => (selecionados/corretos)*100
+        $('.pontuacao span').text(corretosTotal)
+    }
+})
+$('.option-c').click(function () {
+    audio.setAttribute('src', 'assets/audio/acerto.mp3');
+    audio.load();
+    audio.play();
+    corretosTotal++;
+    telaAtual = $(this).parents('.tela');
+    proxTela = $(this).parents('.tela').next();
+    telaAtual.fadeOut();
+    proxTela.fadeIn();
+    if (telaAtual.hasClass('tela-4')) {
+        // resultado => (selecionados/corretos)*100
+        $('.pontuacao span').text(corretosTotal)
+    }
+})
 $('.tente_nv').click(function () {
-    selected = 0;
     corretosTotal = 0;
     $('.tela').fadeOut();
     $('.modal_ingredientes').hide();
     $('.modal_resultado').hide();
     setTimeout(() => {
-        $('.tela-2').fadeIn()
-        $('.tela-1').fadeIn()
+        $('.tela-2').fadeIn();
+        $('.tela-1').fadeIn();
     }, 500)
 })
-$('.verificar_btn').click(function () {
-    let corretos = 0;
-    let telaAtual = $(this).parents('.tela')
-    let modal = telaAtual.children('.modal_resultado')
-    let selecionadosArray = $('.selected')
-
-
-    let errados = 0
-    console.log(selecionadosArray)
-    for (let i = 0; i < selecionadosArray.length; i++) {
-        if (selecionadosArray[i].classList.contains('cr')) {
-            corretos++
-            corretosTotal++
-        } else {
-            errados++
-        }
-    };
-    modal.children('.points').children('span').text(corretos)
-    modal.fadeIn()
-    console.log('errados: ' + errados)
-    console.log('corretos: ' + corretos)
-    console.log(selecionadosArray.length)
-
-})
-$('.next_btn').click(function () {
-    let telaAtual = $(this).parents('.tela')
-    let proxTela = $(this).parents('.tela').next('.tela')
-    telaAtual.fadeOut()
-    setTimeout(() => {
-        proxTela.fadeIn()
-    }, 500)
-    $('.selected').removeClass('selected')
-    selected = 0;
-    if (telaAtual.hasClass('tela-4')) {
-        // resultado => (selecionados/corretos)*100
-        $('.pontuacao span').text((corretosTotal / 16) * 100)
-    }
-})
-
-$('.g-item').click(function () {
-    let item = $(this)
-    if (item.hasClass('selected')) {
-        item.removeClass('selected')
-        selected--
-        console.log('selected sqrs: ' + selected)
-    } else {
-        item.addClass('selected')
-        selected++
-        console.log('selected sqrs: ' + selected)
-    }
-    let verificar_btn = $(this).parents('.section-1').children('.btn_row').children('.verificar_btn');
-    console.log(verificar_btn)
-    if (selected > 0) {
-        verificar_btn.removeClass('disabled');
-    } else if (selected == 0) {
-        verificar_btn.addClass('disabled');
-    }
-})
-
 $('.ingredientes_btn').click(function () {
-    $(this).parents('.tela').children('.modal_ingredientes').fadeIn()
+    $(this).parents('.tela').children('.modal_ingredientes').fadeIn();
 })
 $('.modal_cls_btn').click(function () {
-    $(this).parents('.modal_ingredientes').fadeOut()
+    $(this).parents('.modal_ingredientes').fadeOut();
 })
